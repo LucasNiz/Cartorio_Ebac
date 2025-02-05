7e5d840e971d18a464396a5b4df2e33a72ce13c3
LucasNiz <lucas.muniiiz@gmail.com>
2025-02-05 20:32:39 -0300

    Add files via upload
    Alteração no meu, opção de sair  do sistema "4"

--- cartorio_lucasmuniz.c
+++ cartorio_lucasmuniz.c
@@ -158,7 +158,8 @@ int main()
 		printf("Escolha a opção desejada do menu: \n\n");
 		printf("\t1 - Registrar Nomes \n");
 		printf("\t2 - Consultar Nomes\n");
-		printf("\t3 - Deletar Nomes\n\n");
+		printf("\t3 - Deletar Nomes\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("\tEscolha a Opção Desejada: "); //Fim do Menu
 	
 		scanf("%d",& opcao); //Armazenando a escolha do usuário
@@ -175,7 +176,11 @@ int main()
 				break;
 			case 3:
 				deletar(); //Chama a função de deletar
-				break;			
+				break;	
+			case 4:
+				printf("Obrigado por utilizar o sistema\n");
+				return 0;	
+				break;	
 			default:
 				printf("Você selecionou uma opção inválida, por favor, tente novamente\n"); //Caso selecione uma opção inválida
 				system("pause"); // Esperar a resposta do usuário
